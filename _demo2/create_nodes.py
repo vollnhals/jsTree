b481f3e73b892eddae4c4d6f41ab4648d381b993
Lion Vollnhals <lion.vollnhals@googlemail.com>
2011-04-06 12:32:21 +0200

    fix create_nodes.py. generate valid html. use new nodes html data in demo.

--- _demo2/create_nodes.py
+++ _demo2/create_nodes.py
@@ -1,19 +1,20 @@
 import random
+import sys
 
 idx = 0
 
-for i in range(20):    
-    print "<li rel='drive' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>"
+for i in range(int(sys.argv[1])):    
+    print "<li rel='drive' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>",
     idx += 1
-    print "<ul>"
+    print "<ul>",
     for j in range(random.randint(1, 10)):
-        print "<li rel='folder' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>"
+        print "<li rel='folder' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>",
         idx += 1
-        print "<ul>"
+        print "<ul>",
 
         for k in range(random.randint(1, 10)):
-            print "<li rel='default' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>"
+            print "<li rel='default' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a></li>",
             idx += 1
 
-        print "</ul>"
-    print "</ul>"
+        print "</ul></li>",
+    print "</ul></li>"
