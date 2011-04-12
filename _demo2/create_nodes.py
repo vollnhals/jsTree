a23bbc5b8e7c3d856683e1a55486bfb77f5847c9
Lion Vollnhals <lion.vollnhals@googlemail.com>
2011-04-12 16:45:06 +0200

    create nodes now generates random words and extra spans

--- _demo2/create_nodes.py
+++ _demo2/create_nodes.py
@@ -3,17 +3,23 @@ import sys
 
 idx = 0
 
+f = file("american-words.95", "r")
+words = f.readlines();
+
+def get_word():
+    return words[random.randint(0, len(words) - 1)].strip()
+
 for i in range(int(sys.argv[1])):    
-    print "<li rel='drive' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>",
+    print "<li rel='drive' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>" + get_word() + "</a>" + "<span>" + get_word() + "</span>",
     idx += 1
     print "<ul>",
     for j in range(random.randint(1, 10)):
-        print "<li rel='folder' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a>",
+        print "<li rel='folder' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>" + get_word() + "</a>"+ "<span>" + get_word() + "</span>",
         idx += 1
         print "<ul>",
 
         for k in range(random.randint(1, 10)):
-            print "<li rel='default' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>Node title</a></li>",
+            print "<li rel='default' id='node_" + str(idx) + "'><ins class='jstree-icon'>&#160;</ins><a href=''><ins class='jstree-icon'>&#160;</ins>" + get_word() + "</a>" + "<span>" + get_word() + "</span></li>",
             idx += 1
 
         print "</ul></li>",
