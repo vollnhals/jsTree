c5eaf689ca08b25980a55ee3154d6c4f11d2c49a
Lion Vollnhals <lion.vollnhals@googlemail.com>
2011-04-27 14:00:20 +0200

    set content type to text/json

--- _demo/server/websocket_fallback.php
+++ _demo/server/websocket_fallback.php
@@ -14,7 +14,7 @@ class WebSocketFallback {
 }
 
 header("HTTP/1.0 200 OK");
-header('Content-type: text; charset=utf-8');
+header('Content-type: text/json; charset=utf-8');
 header("Cache-Control: no-cache, must-revalidate");
 header("Expires: Mon, 26 Jul 1997 05:00:00 GMT");
 header("Pragma: no-cache");
